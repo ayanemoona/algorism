b2ea58f171192d9cdd884e423eeda81811ceed2d
Moona <moonapark1126@gmail.com>
2026-04-01 00:02:50 +0900

    최솟값 찾기 혼자 풀이

--- 백준/BOJ_최솟값찾기.py
+++ 백준/BOJ_최솟값찾기.py
@@ -1,2 +1,14 @@
 N,L = map(int,input().split())
 A = list(map(int,input().split()))
+result = []
+for i in range(L-1):
+    A.insert(0,5000000)
+for i in range(N):
+    min = 5000000
+    for j in range(L):
+        if(A[i+j] < min):
+            min = A[i+j]
+    result.append(min)
+    
+print(*result)
+
